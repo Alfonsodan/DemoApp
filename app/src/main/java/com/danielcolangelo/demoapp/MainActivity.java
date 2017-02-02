df610794811b441fd810ae6dadefd35757ef5529
Daniel Colangelo <Danalfons@yahoo.com>
2017-02-02 06:44:19 -0500

    a demo app getting the hang of android studio after being AFK.

---
+++ app/src/main/java/com/danielcolangelo/demoapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.danielcolangelo.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

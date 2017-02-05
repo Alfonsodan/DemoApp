5a887cbd759927d5695c27df62a35673a9fa9ae9
Daniel Colangelo <Danalfons@yahoo.com>
2017-02-05 09:11:21 -0500

    fixed the button so it properly outputs to the log

--- app/src/main/java/com/danielcolangelo/demoapp/MainActivity.java
+++ app/src/main/java/com/danielcolangelo/demoapp/MainActivity.java
@@ -3,10 +3,11 @@ package com.danielcolangelo.demoapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickFunction() {
+    public void clickFunction(View view) {
         Log.i("Info", "Button Pressed");
     }
 
